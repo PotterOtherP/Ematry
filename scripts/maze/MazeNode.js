93869ab4444738f9b2812d5c1e4b9855a2dcc007
nstryon <nstryon@ncsu.edu>
2020-10-29 06:11:49 -0400

    Maze canvas

--- scripts/maze/MazeNode.js
+++ scripts/maze/MazeNode.js
@@ -21,13 +21,16 @@ class MazeNode {
         this.children.push(childNode);
     }
 
-    display()
+    display(ctx)
     {
-        console.log("(" + this.x + ", " + this.y + ")");
+        ctx.fillStyle = "ff0000";
+        // console.log("(" + this.x + ", " + this.y + ")");
+        ctx.fillRect(this.x - 2, this.y - 2, 4, 3);
 
         for (let child of this.children)
         {
-            console.log("(" + child.x + ", " + child.y + ")");
+            // console.log("(" + child.x + ", " + child.y + ")");
+            ctx.fillRect(child.x - 2, child.y - 2, 4, 3);
         }
     }
     
