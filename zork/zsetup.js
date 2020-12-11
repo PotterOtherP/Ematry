b57d36aa11902fbbe6aeb9c8daa42126da1833f1
nstryon <nstryon@ncsu.edu>
2020-12-11 05:17:39 -0500

    Adding maze page, SVG arena

--- zork/zsetup.js
+++ zork/zsetup.js
@@ -1585,7 +1585,7 @@ self.altNames.add("self");
 self.altNames.add("myself");
 // self.takeString = "How romantic!";
 // self.attackString = "You don't have the you.";
-self.eatString = "Auto-cannabalism is not the answer.";
+self.eatString = "Auto-cannibalism is not the answer.";
 
 
 let shaftBasket = new Container("basket", Location.SHAFT_ROOM);
